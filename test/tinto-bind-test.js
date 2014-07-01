64ed037dfadeb74a2c64b0a2ff0a5abcb15748a0
TchernoLeRigolo <weber.jonathan@gmail.com>
2014-07-01 15:55:11 +0200

    Update tinto-bind-test.js

--- test/tinto-bind-test.js
+++ test/tinto-bind-test.js
@@ -1,21 +1,42 @@
 'use strict';
 
 angular.module('TintoBindTest', ['TintoBind'])
-	.controller('TintoBindTestController', function($scope) {
+	.controller('TintoBindTestController', function($timeout, $scope) {
 		$scope.feed = [];
+		$scope.reset = function(newV) {
+			newV = newV || 10;
+			if (newV < $scope.feed.length) {
+				$scope.feed.splice(newV, $scope.feed.length-newV);
+			} else {
+				var L = $scope.feed.length;
+				for (var i=L;i < newV;i++) {
+					var item = {id: i};
+					item.properties = {};
 
-		for (var i=0;i< 100;i++) {
-			var item = {id: i};
-			for (var j=1; j < 20; j++) item['property' + j] = '' + (i < 10 ? '0': '')  + i + ':' + (j < 10 ? '0': '') + j ; 
+					for (var j=1; j < 20; j++) {
+						var v = j < 10 ? '0' + j: j;
+						item.properties['property' + v] = j;
+						item.when = new Date();
+					}
+	
+					$scope.feed.push(item);
+				}
+			}
 
-			$scope.feed.push(item);
+			$timeout(function() {
+				$scope.watchers = countWatchers(angular.element(document.body));
+			}, 10);
 		}
+		$scope.$watch('rows',$scope.reset);
+		$scope.$watch('mode', $scope.reset);
+		$scope.rows = 10;
+		
 
 		var countWatchers = function(root) {
 			var watchers = [];
 
 		    var f = function (element) {
-		        if (element.data().hasOwnProperty('$scope')) {
+		        if (element.data() && element.data().hasOwnProperty('$scope')) {
 		            angular.forEach(element.data().$scope.$$watchers, function (watcher) {
 		                watchers.push(watcher);
 		            });
@@ -28,18 +49,18 @@ angular.module('TintoBindTest', ['TintoBind'])
 
 		    f(root);
 
-		   return watchers.length;
+		    return watchers.length;
 		}
 
-		$scope.countWatchers = function(elemId) {
-			return countWatchers(angular.element(document.getElementById(elemId)));
-		}
-
+		
 		$scope.editItem = function(item) {
-			for (var k in item) {
-				if (k!=='id') item[k] = 'HAHA';
+			for (var k in item.properties) {
+				item.properties[k] = ++item.properties[k];
 			}
+			item.when = new Date();
 		}
+
+		$scope.picture  = 'https://gp3.googleusercontent.com/-O1DdovdVaJQ/AAAAAAAAAAI/AAAAAAAAABs/SHG409tnXZQ/s48-c-k-no/photo.jpg';
 
 		$scope.htmlTest = '<span class="test" style="color:red">This is html</span>';
 	})
